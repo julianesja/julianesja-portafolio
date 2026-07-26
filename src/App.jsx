0c7be07c9b6f7ab34dd01643b0fb16c4e66ec8a6
Julian Estrada <julianesja@gmail.com>
2026-07-26 16:06:12 -0500

    se elimina accounting tools

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,11 @@ import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import Portafolio from "./Screen/Portafolio";
-import AccountingTool from "./Screen/AccountingTool";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Portafolio />,
-  },
-  {
-    path: "/accounting_tools",
-    element: <AccountingTool />,
   },
 ]);
 
